1a3852a2989f089b958a5356ec8a141d31ef9026
ham-myeongyeon <hmy751@gmail.com>
2021-10-01 21:57:05 +0900

    feat: make main page  template

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+const color = {
+  blue: "#83CDDD",
+  white: "#fff",
+  grey: "#F0F2FA",
+};
+
+const padding = {
+  small: "15px",
+  default: "20px",
+  big: "40px",
+};
+
+const theme = {
+  color,
+  padding,
+};
+
+export default theme;
