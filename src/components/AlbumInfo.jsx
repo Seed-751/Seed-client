a848ae57ca27ca44b70223192ba6aad9adba690e
ham-myeongyeon <hmy751@gmail.com>
2021-10-10 15:08:15 +0900

    feat: make function playing audio in BottomPlayer

---
+++ src/components/AlbumInfo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AlbumInfo() {
+  return (
+    <div>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
--- src/pages/MusicDetail.jsx
+++ src/pages/MusicDetail.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+// import jsmediatags from "jsmediatags";
 import styled from "styled-components";
 
 import requestMusic from "../api/requestMuic";
 import BottomPlayer from "../components/BottomPlayer";
+import AlbumInfo from "../components/AlbumInfo";
+import PlayList from "../components/PlayList";
 
 const Container = styled.div`
 `;
@@ -12,6 +15,7 @@ export default function MusicDetail() {
   const params = useParams();
   const musicId = params.music_id;
   const [album, setAlbum] = useState(null);
+  const [currentMusic, setCurrentMusic] = useState(null);
 
   useEffect(() => {
     async function getMusic() {
@@ -19,6 +23,7 @@ export default function MusicDetail() {
         const { data, message } = await requestMusic(musicId);
 
         if (data) {
+          setCurrentMusic(data.audios[0]);
           return setAlbum(data);
         }
 
@@ -31,9 +36,29 @@ export default function MusicDetail() {
     getMusic();
   }, [musicId]);
 
+  // if (album) {
+  //   const temp = album.audios[0];
+  //   console.log("🥰", temp);
+  //   new jsmediatags.Reader(temp), {
+  //     onSuccess: function (tag) {
+  //       console.log(tag);
+  //     }
+  //   };
+  // }
+  // fucntion handleCurrentMusic() {
+  
+  // }
+  if (!album) {
+    return null;
+  }
+
   return (
     <Container>
-      <BottomPlayer />
+      <AlbumInfo />
+      {/* {album?.audios.map((music) => (
+        <PlayList key={music} music={music} />
+      ))} */}
+      {currentMusic && <BottomPlayer music={currentMusic} />}
     </Container>
   );
 }
