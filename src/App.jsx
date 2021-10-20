ad218ed0f1d1e9d800c95e45646b6fb6ec786ce3
ham-myeongyeon <hmy751@gmail.com>
2021-10-20 23:33:53 +0900

    refactor: test logic localstorage token

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ export default function App() {
 
     console.log(token);
     console.log(typeof token);
-    if (!token || token === null) {
+    if (token === null || !token) {
       return handleLoaded(true);
     }
 
