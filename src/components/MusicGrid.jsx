3c5dae53c8a71e3b5694834a07dffd8c4c8f1d36
ham-myeongyeon <hmy751@gmail.com>
2021-10-29 17:10:17 +0900

    refactor: edit musics data to redux store

---
+++ src/components/MusicGrid.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+import { getMusicsRequest, selectMusic } from "../reducers/musicSlice";
+
+import Music from "../components/Music";
+import MoreBox from "../components/MoreBox";
+
+const MusicBox = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  row-gap: 20px;
+  margin-top: 10px;
+
+  .link {
+    width: 250px;
+  }
+`;
+
+export default function MusicGrid() {
+  const [page, setPage] = useState(0);
+  const { musics } = useSelector(selectMusic);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMusicsRequest(page));
+  }, [page, dispatch]);
+
+  function handlePageScroll() {
+    setPage((page) => page + 1);
+  }
+
+  return (
+    <>
+      <MusicBox>
+        {musics?.map((music) => (
+          <Link
+            key={music._id}
+            to={`/musics/${music._id}`}
+            className="link"
+          >
+            <Music key={music._id} music={music} />
+          </Link>
+        ))}
+      </MusicBox>
+      <MoreBox handlePageScroll={handlePageScroll} />
+    </>
+  );
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import React from "react";
 import styled from "styled-components";
 
 import requestMusics from "../api/requestMusics";
-import { occurError } from "../reducers/errorSlice";
 
-import Music from "../components/Music";
-import MoreBox from "../components/MoreBox";
+import MusicGrid from "../components/MusicGrid";
 
-const Container = styled.div`
+const Wrapper = styled.div`
   height: 100%;
   padding: 10px;
 
@@ -18,65 +14,11 @@ const Container = styled.div`
   }
 `;
 
-const MusicBox = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  row-gap: 20px;
-  margin-top: 10px;
-
-  .link {
-    width: 250px;
-  }
-`;
-
 export default function Dashboard() {
-  const [page, setPage] = useState(0);
-  const [list, setList] = useState([]);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (page === 0) {
-      return null;
-    }
-
-    async function getMusics() {
-      try {
-        const { data, message } = await requestMusics(page);
-
-        if (data) {
-          return setList((list) => [...list, ...data]);
-        }
-
-        dispatch(occurError(message));
-      } catch (err) {
-        dispatch(occurError(err.message));
-      }
-    }
-
-    getMusics();
-  }, [page]);
-
-  function handlePageScroll() {
-    setPage((page) => page + 1);
-  }
-
   return (
-    <>
-      <Container>
-        <h1 className="title">Albums</h1>
-        <MusicBox>
-          {list?.map((music) => (
-            <Link
-              key={music._id}
-              to={`/musics/${music._id}`}
-              className="link"
-            >
-              <Music key={music._id} music={music} />
-            </Link>
-          ))}
-        </MusicBox>
-      </Container>
-      <MoreBox handlePageScroll={handlePageScroll} />
-    </>
+    <Wrapper>
+      <h1 className="title">Albums</h1>
+      <MusicGrid apiMethod={requestMusics} />
+    </Wrapper>
   );
 }
