21fba7e9c64a706b43c13a441eed430dd373dab8
ham-myeongyeon <hmy751@gmail.com>
2021-11-13 21:21:56 +0900

    feat: add test case about Signup Form

---
+++ src/__test__/components/SignupForm.test.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import MockProvider from "../MockProvider";
+import SignupForm from "../../components/SignupForm";
+
+describe("SignupForm component", () => {
+  beforeEach(() => {
+    render(
+      <MockProvider>
+        <SignupForm />
+      </MockProvider>
+    );
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it("has input component ", () => {
+    const profileImageInput = screen.getByLabelText("Profile Image");
+    const emailInput = screen.getByLabelText("Email");
+    const passwordInput = screen.getByLabelText("Password");
+    const passwordConfirmInput = screen.getByLabelText("Password confirm");
+    const nameInput = screen.getByLabelText("Name");
+
+    expect(profileImageInput).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(passwordConfirmInput).toBeInTheDocument();
+    expect(nameInput).toBeInTheDocument();
+  });
+
+  it("change text input", () => {
+    const emailInput = screen.getByLabelText("Email");
+    const passwordInput = screen.getByLabelText("Password");
+    const passwordConfirmInput = screen.getByLabelText("Password confirm");
+    const nameInput = screen.getByLabelText("Name");
+
+    userEvent.type(emailInput, "test@gmail.com");
+    userEvent.type(passwordInput, "test");
+    userEvent.type(passwordConfirmInput, "test");
+    userEvent.type(nameInput, "test");
+
+    expect(emailInput).toHaveValue("test@gmail.com");
+    expect(passwordInput).toHaveValue("test");
+    expect(passwordConfirmInput).toHaveValue("test");
+    expect(nameInput).toHaveValue("test");
+  });
+
+  it("upload image file", () => {
+    const profileImageInput = screen.getByLabelText("Profile Image");
+    const file = new File(["test"], "test.png", { type: "image/png" });
+    global.URL.createObjectURL = jest.fn();
+
+    userEvent.upload(profileImageInput, file);
+
+    expect(profileImageInput.files[0]).toStrictEqual(file);
+    expect(profileImageInput.files).toHaveLength(1);
+  });
+});
