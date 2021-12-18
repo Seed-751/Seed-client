65d37c2b08d223a362bd96cb0e57d363c4494183
myeongyeon <hmy751@gmail.com>
2021-12-19 00:30:42 +0900

    Style: edit flex gap in LoginForm

--- src/components/Pages/Login/LoginForm.jsx
+++ src/components/Pages/Login/LoginForm.jsx
@@ -20,8 +20,6 @@ const Form = styled.form`
 const InputBox = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 5px;
-  margin-top: 10px;
   margin-bottom: 10px;
   padding: 10px 0;
   width: 400px;
